b957507cd181946c25522ecc56ecdfb9e9d5afe9
Ryan Godfrey <ryangodfrey@Ryans-MacBook-Pro.local>
2025-03-19 11:15:32 +1000

    repo 3

---
+++ 1.1.R
@@ -0,0 +1,14 @@
+getwd(
+)
+install.packages("use_github")
+
+usethis::usegithub()
+install.packages("usethis")
+usethis:use_github()
+usethis::use_github()
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+usethis::use_github()
+usethis::use_github()
+gitcreds::gitcreds_set()
+usethis::use_github()
+usethis::use_github()
